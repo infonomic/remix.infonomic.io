d16ec46275e2734bde51a453386a67297b42da48
Anthony Bouch <anthony@infonomic.io>
2023-01-09 12:21:47 +0700

    feat: crazy mardown about page

--- app/routes/_public+/about.tsx
+++ app/routes/_public+/about.tsx
@@ -1,4 +1,14 @@
+import * as React from 'react'
+import ReactMarkdown from 'react-markdown'
+
+import { json } from '@remix-run/node'
+import type { LoaderArgs } from '@remix-run/node'
+import { useCatch, useLoaderData } from '@remix-run/react'
+
+import remarkGfm from 'remark-gfm'
 import { mergeMeta } from '~/utils/utils'
+
+import ErrorLayout from '~/ui/layouts/error-layout'
 
 /**
  * meta
@@ -15,16 +25,56 @@ export const meta = ({ matches }: any) => {
 }
 
 /**
+ * loader
+ * @param param0
+ * @returns
+ */
+export async function loader() {
+  const response = await fetch(
+    'https://raw.githubusercontent.com/infonomic/remix.infonomic.io/develop/README.md'
+  )
+  const text = await response.text()
+  return json({ text })
+}
+
+/**
  * AboutInPage
  * @returns
  */
 export default function AboutPage() {
+  const data = useLoaderData()
+
   return (
-    <article className="mx-auto mt-[1rem] max-w-[960px] md:mt-[3rem] ">
-      <div className="prose dark:prose-invert">
+    <article className="prose mx-auto mt-[1rem] max-w-[960px] dark:prose-invert md:mt-[3rem] ">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{data.text}</ReactMarkdown>
+      {/* <div className="prose dark:prose-invert">
         <h1 className="mb-5 text-[2.25rem]">About</h1>
       </div>
-      <div>Article here...</div>
+      <div>Article here...</div> */}
     </article>
   )
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  // eslint-disable-next-line no-console
+  console.error(error)
+
+  return <div>An unexpected error occurred: {error.message}</div>
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+
+  if (caught.status === 404) {
+    return (
+      <ErrorLayout>
+        <div>
+          <h1>Not Found</h1>
+          <p>Oops. About content note found.</p>
+        </div>
+      </ErrorLayout>
+    )
+  }
+
+  throw new Error(`Unexpected caught response with status: ${caught.status}`)
 }
