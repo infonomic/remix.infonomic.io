6a981dcc952b0f1ae5fac86711f3379b7904909a
Anthony Bouch <anthony@infonomic.io>
2023-01-18 09:13:08 +0700

    feat: router-pager preserves other search params

--- app/ui/components/pager/router-pager.tsx
+++ app/ui/components/pager/router-pager.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@remix-run/react'
+import { useSearchParams } from '@remix-run/react'
 
 import { Pagination, FirstIcon, PreviousIcon, NextIcon, LastIcon } from '~/ui/components/pager'
 import type { PaginationProps } from '~/ui/components/pager'
@@ -11,79 +12,96 @@ import type { PaginationProps } from '~/ui/components/pager'
  * Nest, Last buttons etc.)
  */
 export const RouterPager = (props: PaginationProps) => {
+  const [searchParams] = useSearchParams()
+
   return (
     <Pagination {...props}>
       <Pagination.Root>
         <Pagination.Pager
-          renderFirst={(key, item) => (
-            <Pagination.First asChild key={key} disabled={item.disabled}>
-              {item.disabled ? (
-                <div>
-                  <FirstIcon />
-                </div>
-              ) : (
-                <Link to=".">
-                  <FirstIcon />
-                </Link>
-              )}
-            </Pagination.First>
-          )}
-          renderPrevious={(key, item) => (
-            <Pagination.Previous asChild key={key} disabled={item.disabled}>
-              {item.disabled ? (
-                <div>
-                  <PreviousIcon />
-                </div>
-              ) : (
-                <Link to={`.?page=${item.page - 1}`}>
-                  <PreviousIcon />
-                </Link>
-              )}
-            </Pagination.Previous>
-          )}
-          renderPageNumber={(key, item) => (
-            <Pagination.PageNumber
-              asChild
-              key={key}
-              page={item.page}
-              selected={item.selected}
-              disabled={item.disabled}
-              activeClassName="active"
-            >
-              {item.disabled ? (
-                <div>{item.page}</div>
-              ) : (
-                <Link to={`.?page=${item.page}`}>{item.page}</Link>
-              )}
-            </Pagination.PageNumber>
-          )}
-          renderNext={(key, item) => (
-            <Pagination.Next asChild key={key} page={item.page} disabled={item.disabled}>
-              {item.disabled ? (
-                <div>
-                  <NextIcon />
-                </div>
-              ) : (
-                <Link to={`.?page=${item.page + 1}`}>
-                  <NextIcon />
-                </Link>
-              )}
-            </Pagination.Next>
-          )}
-          renderLast={(key, item, count) => (
-            <Pagination.Last asChild key={key} disabled={item.disabled} count={count}>
-              {item.disabled ? (
-                <div>
-                  {' '}
-                  <LastIcon />
-                </div>
-              ) : (
-                <Link to={`.?page=${count}`}>
-                  <LastIcon />
-                </Link>
-              )}
-            </Pagination.Last>
-          )}
+          renderFirst={(key, item) => {
+            searchParams.delete('page')
+            return (
+              <Pagination.First asChild key={key} disabled={item.disabled}>
+                {item.disabled ? (
+                  <div>
+                    <FirstIcon />
+                  </div>
+                ) : (
+                  <Link to={`.?${searchParams.toString()}`}>
+                    <FirstIcon />
+                  </Link>
+                )}
+              </Pagination.First>
+            )
+          }}
+          renderPrevious={(key, item) => {
+            searchParams.set('page', (item.page - 1).toString())
+            return (
+              <Pagination.Previous asChild key={key} disabled={item.disabled}>
+                {item.disabled ? (
+                  <div>
+                    <PreviousIcon />
+                  </div>
+                ) : (
+                  <Link to={`.?${searchParams.toString()}`}>
+                    <PreviousIcon />
+                  </Link>
+                )}
+              </Pagination.Previous>
+            )
+          }}
+          renderPageNumber={(key, item) => {
+            searchParams.set('page', item.page.toString())
+            return (
+              <Pagination.PageNumber
+                asChild
+                key={key}
+                page={item.page}
+                selected={item.selected}
+                disabled={item.disabled}
+                activeClassName="active"
+              >
+                {item.disabled ? (
+                  <div>{item.page}</div>
+                ) : (
+                  <Link to={`.?${searchParams.toString()}`}>{item.page}</Link>
+                )}
+              </Pagination.PageNumber>
+            )
+          }}
+          renderNext={(key, item) => {
+            searchParams.set('page', (item.page + 1).toString())
+            return (
+              <Pagination.Next asChild key={key} page={item.page} disabled={item.disabled}>
+                {item.disabled ? (
+                  <div>
+                    <NextIcon />
+                  </div>
+                ) : (
+                  <Link to={`.?${searchParams.toString()}`}>
+                    <NextIcon />
+                  </Link>
+                )}
+              </Pagination.Next>
+            )
+          }}
+          renderLast={(key, item, count) => {
+            searchParams.set('page', count.toString())
+            return (
+              <Pagination.Last asChild key={key} disabled={item.disabled} count={count}>
+                {item.disabled ? (
+                  <div>
+                    {' '}
+                    <LastIcon />
+                  </div>
+                ) : (
+                  <Link to={`.?${searchParams.toString()}`}>
+                    <LastIcon />
+                  </Link>
+                )}
+              </Pagination.Last>
+            )
+          }}
         />
       </Pagination.Root>
     </Pagination>
